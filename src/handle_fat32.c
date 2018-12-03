94ea427035829fa4584a853987116c048001a0b5
Louis-marie JOLY <ljoly@e2r7p4.42.fr>
2018-12-03 13:59:37 +0100

    fat32 ok

--- src/handle_fat32.c
+++ src/handle_fat32.c
@@ -6,7 +6,7 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 19:31:58 by ljoly             #+#    #+#             */
-/*   Updated: 2018/11/30 17:08:42 by ljoly            ###   ########.fr       */
+/*   Updated: 2018/12/03 13:56:16 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 #include "handle_memory.h"
 #include "handle_magic.h"
 
-static t_file	get_mach_o_file(void *file, struct fat_arch *arch)
+static t_file	get_mach_o_file(void *file, struct fat_arch *arch, t_bool swap)
 {
 	t_file		mach_o;
 
-	// printf("ptr = %p\n", file + arch->offset);
-	mach_o.ptr = file + arch->offset;
-	mach_o.size = arch->size;
+	mach_o.ptr = file + swap_32(arch->offset, swap);
+	mach_o.size = swap_32(arch->size, swap);
 	return (mach_o);
 }
 
@@ -45,35 +44,15 @@ t_bool			handle_fat32(t_file f, const char *arg)
 			return (FALSE);
 		if ((swap_32(arch->cputype, f.swap) == CPU_TYPE_X86_64))
 		{
-			ft_putendl("CPU FOUND");
-			if (!access_at(f, f.ptr + arch->offset))
+			if (!access_at(f, f.ptr + swap_32(arch->offset, f.swap)))
 				return (FALSE);
-			mach_o = get_mach_o_file(f.ptr, arch);
-			ft_putendl("HAHAHA");
+			mach_o = get_mach_o_file(f.ptr, arch, f.swap);
 			magic = *(int *)mach_o.ptr;
 			handle_magic(magic, mach_o, arg);
 			break ;
 		}
-		// else
-			// ft_printf("cpu_type = %zu\n", swap_32(arch->cputype, swap));
 		arch = (void*)arch + sizeof(*arch);
 		i++;
 	}
 	return (TRUE);
-	// bin.lc = g_file + sizeof(struct mach_header_64);
-	// bin.sects = NULL;
-	// bin.nsects = 0;
-	// i = 0;
-	// while (i < bin.header->ncmds)
-	// {
-	// 	if (!is_consistent(&bin, &check_sects_64))
-	// 		return (free_memory(bin.sects, NULL, FALSE));
-	// 	bin.lc = (void *)bin.lc + bin.lc->cmdsize;
-	// 	i++;
-	// }
-	// if (bin.nsects * sizeof(struct section_64) >= g_size)
-	// 	return (free_memory(bin.sects, NULL, FALSE));
-	// get_sections_64(&bin, g_file);
-	// bin.lc = g_file + sizeof(struct mach_header_64);
-	// return (handle_syms(&bin, &get_syms64));
 }
