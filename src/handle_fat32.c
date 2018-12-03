e06a33cec2c4c45952fafcc6facc07f548966e77
Louis-marie JOLY <ljoly@e2r7p4.42.fr>
2018-12-03 14:28:01 +0100

    norme

--- src/handle_fat32.c
+++ src/handle_fat32.c
@@ -6,11 +6,12 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 19:31:58 by ljoly             #+#    #+#             */
-/*   Updated: 2018/12/03 14:13:39 by ljoly            ###   ########.fr       */
+/*   Updated: 2018/12/03 14:27:40 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nm.h"
+#include "fat.h"
 #include "handle_memory.h"
 #include "handle_magic.h"
 
@@ -25,33 +26,28 @@ static t_file	get_mach_o_file(void *file, struct fat_arch *arch, t_bool swap)
 
 t_bool			handle_fat32(t_file f, const char *arg)
 {
-	struct fat_header	*header;
-	struct fat_arch		*arch;
-	t_file				mach_o;
-	int					magic;
-	uint32_t			i;
+	t_fat		fat;
+	uint32_t	i;
 
-	header = (struct fat_header *)f.ptr;
-	if (!access_at(f, f.ptr + sizeof(*header)))
-	{
+	fat.header = (struct fat_header *)f.ptr;
+	if (!access_at(f, f.ptr + sizeof(*fat.header)))
 		return (FALSE);
-	}
-	arch = f.ptr + sizeof(*header);
+	fat.arch = f.ptr + sizeof(*fat.header);
 	i = 0;
-	while (i < swap_32(header->nfat_arch, f.swap))
+	while (i < swap_32(fat.header->nfat_arch, f.swap))
 	{
-		if (!access_at(f, (void*)arch + sizeof(*arch)))
+		if (!access_at(f, (void*)fat.arch + sizeof(*fat.arch)))
 			return (FALSE);
-		if ((swap_32(arch->cputype, f.swap) == CPU_TYPE_X86_64))
+		if ((swap_32(fat.arch->cputype, f.swap) == CPU_TYPE_X86_64))
 		{
-			if (!access_at(f, f.ptr + swap_32(arch->offset, f.swap)))
+			if (!access_at(f, f.ptr + swap_32(fat.arch->offset, f.swap)))
 				return (FALSE);
-			mach_o = get_mach_o_file(f.ptr, arch, f.swap);
-			magic = *(int *)mach_o.ptr;
-			handle_magic(magic, mach_o, arg);
+			fat.mach_o = get_mach_o_file(f.ptr, fat.arch, f.swap);
+			fat.magic = *(int *)fat.mach_o.ptr;
+			handle_magic(fat.magic, fat.mach_o, arg);
 			break ;
 		}
-		arch = (void*)arch + sizeof(*arch);
+		fat.arch = (void*)fat.arch + sizeof(*fat.arch);
 		i++;
 	}
 	return (TRUE);
