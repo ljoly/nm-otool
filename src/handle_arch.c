b8bb8f0635f207313a92a6420126fee4b395b2ae
Louis-marie JOLY <ljoly@e2r4p3.42.fr>
2018-12-04 18:34:32 +0100

    wip archives

--- src/handle_arch.c
+++ src/handle_arch.c
@@ -6,7 +6,7 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 16:30:39 by ljoly             #+#    #+#             */
-/*   Updated: 2018/12/03 19:42:10 by ljoly            ###   ########.fr       */
+/*   Updated: 2018/12/04 18:30:31 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,28 @@
 t_bool			handle_arch(t_file f, const char *arg)
 {
 	void			*p;
-	struct ar_hdr	*ar;
+	// struct ar_hdr	*ar;
 	struct ranlib	*ranlib;
-	uint64_t		ranlib_size;
-	uint64_t		i;
+	uint32_t		ranlib_size;
+	uint32_t		i;
 
 	(void)arg;
-	p = f.ptr + SARMAG + sizeof(*ar);
-	// ft_printf("sizeof struct ar = %zu && p = %u\n", sizeof(*ar), p);
-	ar = f.ptr + SARMAG;
-	ft_putendl(ar->ar_fmag);
+	p = f.ptr + SARMAG + sizeof(struct ar_hdr) + 20;
 	if (!access_at(f, p))
 		return (FALSE);
-	ranlib_size = *(uint64_t*)p;
-	p = p + sizeof(uint64_t);
+	ranlib_size = *(uint32_t*)p / sizeof(struct ranlib);
+	p = p + sizeof(uint32_t);
 	ranlib = (struct ranlib*)p;
 	i = 0;
-	ft_printf("size = %llu\n", ranlib_size);
 	while (i < ranlib_size)
 	{
-		if (!access_at(f, ranlib + sizeof(*ranlib)))
-			return (FALSE);
-		ranlib = (void*)ranlib + sizeof(*ranlib);
+		// if (!access_at(f, ranlib + sizeof(*ranlib)))
+		// 	return (FALSE);
+		// ft_printf("%d: %x && off = %x\n", i, ranlib[i].ran_un.ran_strx, ranlib[i].ran_off);
+		p = f.ptr + ranlib[i].ran_off + sizeof(struct ar_hdr);
+		ft_printf("offset = %u: %s\n", ranlib[i].ran_off, (char*)p);
+		// ranlib = (void*)ranlib + sizeof(*ranlib);
+		// exit(0);
 		i++;
 	}
 	return (TRUE);
