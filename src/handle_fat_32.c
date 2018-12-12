6f8e41a42ee4bbc3e12207cf3158f14241cf9992
Louis-marie JOLY <ljoly@e2r10p5.42.fr>
2018-12-12 19:38:13 +0100

    handle 32bit architectures in fat32

--- src/handle_fat_32.c
+++ src/handle_fat_32.c
@@ -6,7 +6,7 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 19:31:58 by ljoly             #+#    #+#             */
-/*   Updated: 2018/12/11 20:56:02 by ljoly            ###   ########.fr       */
+/*   Updated: 2018/12/12 19:14:44 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,11 @@ static t_file	get_mach_o_file(void *file, struct fat_arch *arch, t_bool swp)
 	return (mach_o);
 }
 
-static t_bool	handle_ppc(t_file f, t_fat_32 fat, const char *arg)
+static t_bool	handle_32bit_cpu(t_file f, t_fat_32 fat, const char *arg)
 {
 	uint32_t	i;
+	uint32_t	j;
+	char		*cpu_type;
 
 	i = 0;
 	fat.arch = f.ptr + sizeof(struct fat_header);
@@ -34,8 +36,17 @@ static t_bool	handle_ppc(t_file f, t_fat_32 fat, const char *arg)
 	{
 		if (!access_at(f, f.ptr + swp32(&fat.arch->offset, f.swp)))
 			return (FALSE);
+		j = 0;
+		cpu_type = NULL;
+		while (j < sizeof(g_cpu32) / sizeof(*g_cpu32))
+		{
+			if (fat.arch->cputype == g_cpu32[j].type)
+				cpu_type = g_cpu32[j].name;
+			j++;
+		}
 		fat.mach_o = get_mach_o_file(f.ptr, fat.arch, f.swp);
 		fat.magic = *(int *)fat.mach_o.ptr;
+		ft_printf("\n%s (for architecture %s):\n", arg, cpu_type);
 		handle_magic(fat.magic, fat.mach_o, arg);
 		fat.arch = (void*)fat.arch + sizeof(*fat.arch);
 		i++;
@@ -64,7 +75,7 @@ t_bool			handle_fat_32(t_file f, const char *arg)
 	{
 		if (!access_at(f, (void*)fat.arch + sizeof(*fat.arch)))
 			return (FALSE);
-		if ((swp_signed32(&fat.arch->cputype, f.swp) == CPU_TYPE_X86_64))
+		if (is_cpu_64(swp_signed32(&fat.arch->cputype, f.swp)))
 		{
 			fat.cpu_type_found = TRUE;
 			if (!access_at(f, f.ptr + swp32(&fat.arch->offset, f.swp)))
@@ -78,6 +89,6 @@ t_bool			handle_fat_32(t_file f, const char *arg)
 		i++;
 	}
 	if (!fat.cpu_type_found)
-		return (handle_ppc(f, fat, arg));
+		return (handle_32bit_cpu(f, fat, arg));
 	return (TRUE);
 }
