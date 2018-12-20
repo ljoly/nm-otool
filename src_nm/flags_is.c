62f161fd6f58d082713c0b032de602ea94b96fd8
Louis-marie JOLY <ljoly@e2r10p5.42.fr>
2018-12-20 13:44:12 +0100

    refacto flag handling

--- src_nm/flags_is.c
+++ src_nm/flags_is.c
@@ -6,7 +6,7 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 19:14:11 by ljoly             #+#    #+#             */
-/*   Updated: 2018/12/19 19:36:09 by ljoly            ###   ########.fr       */
+/*   Updated: 2018/12/20 13:42:13 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 t_bool		is_only_undf(void)
 {
-	return (g_flags & 1);
+	return (g_flags & ONLY_UNDF);
 }
 
 t_bool		is_no_undf(void)
 {
-	return ((g_flags >> 1) & 1);
+	return (g_flags & NO_UNDF);
 }
 
 t_bool		is_only_names(void)
 {
-	return ((g_flags >> 2) & 1);
+	return (g_flags & ONLY_NAMES);
 }
 
 t_bool		is_no_sort(void)
 {
-	return ((g_flags >> 3) & 1);
+	return (g_flags & NO_SORT);
 }
 
 t_bool		is_only_extern(void)
 {
-	return ((g_flags >> 4) & 1);
+	return (g_flags & ONLY_EXTERN);
 }
