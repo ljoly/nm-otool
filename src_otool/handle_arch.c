5b592c37bab14250fee1bf5ba31eacae71817131
Louis-marie JOLY <ljoly@e2r10p5.42.fr>
2019-01-03 14:30:53 +0100

    norme

--- src_otool/handle_arch.c
+++ src_otool/handle_arch.c
@@ -6,7 +6,7 @@
 /*   By: ljoly <ljoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 16:30:39 by ljoly             #+#    #+#             */
-/*   Updated: 2018/12/26 19:16:03 by ljoly            ###   ########.fr       */
+/*   Updated: 2019/01/03 14:07:33 by ljoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,8 +77,9 @@ t_bool					handle_arch(t_file f, const char *arg)
 	{
 		magic = *(uint32_t*)mach_o.ptr;
 		mach_o.size = ft_atoi(ar->ar_size);
-		ar_name_size ? ft_printf("%s(%s):\n", arg, (char*)ar + sizeof(struct ar_hdr)) :
-			ft_printf("%s(%s):\n", arg, ar->ar_name);
+		ar_name_size ? ft_printf("%s(%s):\n", arg,
+			(char*)ar + sizeof(struct ar_hdr)) :
+				ft_printf("%s(%s):\n", arg, ar->ar_name);
 		handle_magic_otool(magic, mach_o, arg, FALSE);
 		ar = (void*)ar + sizeof(struct ar_hdr) + ft_atoi(ar->ar_size);
 		if (!access_at(f, (void*)ar + sizeof(struct ar_hdr)))
